fb1d377fa1591dbb899fa76da0b8cfdf895fab9c
raylu <90059+raylu@users.noreply.github.com>
2025-05-02 23:10:27 -0700

    parse all SGFs

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,37 @@ use std::fs;
 async fn main() {
 	let app = axum::Router::new()
 		.fallback_service(tower_http::services::ServeFile::new("frontend/index.html"))
+		.route("/search", get(search))
 		.nest_service("/sgf", tower_http::services::ServeDir::new("game_records"))
 		.nest_service("/static", tower_http::services::ServeDir::new("static"));
 	let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 	axum::serve(listener, app).await.unwrap();
 }
 
-fn info() {
-	let path = "game_records/2024 KifuDepot Games/2024-08-02 張羽喬 vs Liu Yifang.sgf";
-	let game = parse_sgf(path).unwrap();
-	let black = match game[0].get_property("PB").unwrap() {
-		Prop::PB(st) => st,
-		_ => unreachable!(),
-	};
-	let white = match game[0].get_property("PW").unwrap() {
-		Prop::PW(st) => st,
-		_ => unreachable!(),
-	};
-	println!("black: {}, white: {}", black, white);
+async fn search() -> String {
+	let sgf_paths = walkdir::WalkDir::new("game_records")
+		.into_iter()
+		.filter_map(|e| e.ok())
+		.filter(|e| e.file_type().is_file())
+		.filter_map(|e| e.path().to_str().map(|s| s.to_string()))
+		.filter(|s| s.ends_with(".sgf"));
+	for path in sgf_paths {
+		match parse_sgf(&path) {
+			Ok(game) => {
+				let black = match game[0].get_property("PB").unwrap() {
+					Prop::PB(st) => st,
+					_ => unreachable!(),
+				};
+				let white = match game[0].get_property("PW").unwrap() {
+					Prop::PW(st) => st,
+					_ => unreachable!(),
+				};
+				println!("black: {}, white: {}", black, white);
+			},
+			Err(e) => println!("Error parsing {}: {}", path, e),
+		}
+	}
+	"done".to_string()
 }
 
 fn parse_sgf<P: AsRef<std::path::Path> + ?Sized>(path: &P) -> Result<Vec<SgfNode<Prop>>, Box<dyn std::error::Error>> {
