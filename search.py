7d826f7f317dd7913966c37506b355f39c6e6f01
raylu <90059+raylu@users.noreply.github.com>
2025-05-03 23:06:48 -0700

    use libkombilo to index an SGF

--- search.py
+++ search.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 
-import zipfile
+import pathlib
 
-import pysgf
+import kombilo
 
 def main():
-	zipf = zipfile.ZipFile('aesalon.zip', 'r')
-	for i, filename in enumerate(zipf.namelist()):
-		if not filename.endswith('.sgf'):
-			continue
-		print(filename)
-		sgf = pysgf.SGF.parse(zipf.read(filename).decode())
-		print(sgf.board_size, sgf.komi, sgf.ruleset)
-		print(sgf.properties)
-		print(sgf.player)
-		if i >= 10: break
+	opts = kombilo.ProcessOptions()
+	opts.sgfInDB = False
+	opts.processVariations = False
+	opts.professional_tag = 2 # tag if 1p-9p
+	gamelist = kombilo.GameList("game_records/kombilo.db", "", "", opts)
+
+	root = pathlib.Path('game_records')
+	glists = kombilo.vectorGL()
+	for subdir, _, filenames in root.walk():
+		for filename in filenames:
+			if not filename.endswith('.sgf'): continue
+			path = (subdir / filename)
+			print('processing', path)
+			gamelist.process(path.read_bytes(), subdir.as_posix(), filename, glists)
+			break
 
 if __name__ == "__main__":
     main()
